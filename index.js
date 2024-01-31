a4b6b8ecc661c60a3a8d8bdd577fc9bb7f21ab46
Velicko00 <stefan.velickovic00@gmail.com>
2024-01-31 18:16:52 +0100

    definisane rute i modeli

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
 
 const connect = async () => {
     try {
-      await mongoose.connect(process.env.MONGO);
-      console.log("Connected to mongoDB.");
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB.");
     } catch (error) {
-      throw error;
+        throw error;
     }
-  };
+};
 
-  mongoose.connection.on("disconnected", () => {
+mongoose.connection.on("disconnected", () => {
     console.log("mongoDB disconnected!");
-  });
+});
+
+app.use("/auth", authRoute);
+app.use("/users", usersRoute);
+app.use("/hotels", hotelsRoute);
+app.use("/rooms", roomsRoute);
 
 app.listen(8800, () => {
     connect();
     console.log("Connected to backend.");
-  });
+});
