3b23f0064e66c9858dc9cbed9098831125555684
Velicko00 <stefan.velickovic00@gmail.com>
2024-02-01 03:07:05 +0100

    create, update, delete, get, getall hotel

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ mongoose.connection.on("disconnected", () => {
     console.log("mongoDB disconnected!");
 });
 
+app.use(express.json());
+
 app.use("/auth", authRoute);
 app.use("/users", usersRoute);
 app.use("/hotels", hotelsRoute);
